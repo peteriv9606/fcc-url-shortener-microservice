9d6689346d4f50c50cb348db99bc27eee13e441b
Petar Ivanov <peteriv9606@gmail.com>
2021-06-10 13:13:44 +0300

    showing only original_url and short_url in query; redirecting to correct url

--- server.js
+++ server.js
@@ -61,7 +61,8 @@ app.get("/api/shorturl/:input", (req, res) => {
     } else {
       //data[0].original_url this is what we need
       try {
-        res.send(data[0].original_url);
+        //console.log(data);
+        res.redirect(data[0].original_url);
       } catch (err) {
         //console.error(err);
         res.json({ error: "invalid url" });
@@ -71,49 +72,61 @@ app.get("/api/shorturl/:input", (req, res) => {
 });
 
 app.post("/api/shorturl/", (req, res) => {
-  dns.resolve(req.body.url, (err) => {
-    //1. check to see if input is a valid url
-    //2. if input is ok - check to see if input is in database
-    //3. if input is not in database - assign a new short link, otherwise - return current short link (object)
-    if (err) {
-      console.error(err);
-      res.json({ error: "invalid url" });
-    } else {
-      //try and find it in database
-      var highest = 0;
-      var existing = null;
-      UrlModel.find({}, (error, data) => {
-        if (error) {
-          res.json({ error: error });
-        }
-        //determine which is the highest taken short link (number)
-        data.map((el) => {
-          if (highest < el.short_url) highest = el.short_url;
-          if (el.original_url === req.body.url) existing = el;
-        });
-        // Find if the array (data) contains the url by comparing the property value (req.body.url)
-        if (existing) {
-          //object existing - display object
-          res.json(existing);
-        } else {
-          //object not found - create a new object with the highest + 1 for short_link
-          //display newly created object
-          var newLink = new UrlModel({
-            original_url: req.body.url,
-            short_url: highest + 1,
-          });
-          newLink.save((err, result) => {
-            if (err) {
-              res.json({ errorCreatingRecord: err });
-            } else {
-              console.log("displaying newly created record..");
-              res.json(result);
+  //ok - https://www.google.com http://www.google.com
+  try {
+    //parse input to URL - if parse is okay - input is valid url format
+    var myUrl = new URL(req.body.url);
+
+    dns.resolve(myUrl.host, (err) => {
+      //1. check to see if input is a valid url
+      //2. if input is ok - check to see if input is in database
+      //3. if input is not in database - assign a new short link, otherwise - return current short link (object)
+      if (err) {
+        console.error(err);
+        res.json({ error: "invalid url" });
+      } else {
+        //try and find it in database
+        var highest = 0;
+        var existing = null;
+        UrlModel.find(
+          {},
+          { original_url: true, short_url: true, _id: false },
+          (error, data) => {
+            if (error) {
+              res.json({ error: error });
             }
-          });
-        }
-      });
-    }
-  });
+            //determine which is the highest taken short link (number)
+            data.map((el) => {
+              if (highest < el.short_url) highest = el.short_url;
+              if (el.original_url === req.body.url) existing = el;
+            });
+            // Find if the array (data) contains the url by comparing the property value (req.body.url)
+            if (existing) {
+              //object existing - display object
+              res.json(existing);
+            } else {
+              //object not found - create a new object with the highest + 1 for short_link
+              //display newly created object
+              var newLink = new UrlModel({
+                original_url: myUrl.protocol + "//" + myUrl.host,
+                short_url: highest + 1,
+              });
+              newLink.save((err, result) => {
+                if (err) {
+                  res.json({ errorCreatingRecord: err });
+                } else {
+                  console.log("displaying newly created record..");
+                  res.json(result);
+                }
+              });
+            }
+          }
+        );
+      }
+    });
+  } catch (error) {
+    res.json({ error: "invalid url" });
+  }
 });
 
 app.post("/api/shorturl", (req, res) => {});
